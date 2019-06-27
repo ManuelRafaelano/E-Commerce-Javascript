5d0e57b8b5f76cd19e3543f769dfa141635c2041
Manuel Alexander Vasquez Rafaelano <manuelrafaelano@gmail.com>
2019-06-27 16:03:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,22 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if (inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      } else {
+        button.addEventListener("click", event => {
+          event.target.innerText = "In Cart";
+          event.target.disabled = true;
+        });
+      }
+    });
+  }
 }
 //local storage
 class Storage {
@@ -67,7 +83,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
   //get all products
-  products.getProducts().then(products => {
-    ui.displayProducts(products), Storage.saveProducts(products);
-  });
+  products
+    .getProducts()
+    .then(products => {
+      ui.displayProducts(products), Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
