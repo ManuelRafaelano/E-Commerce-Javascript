6fd9283c8467630b2af444cceb29dc7aa679877e
Manuel Alexander Vasquez Rafaelano <manuelrafaelano@gmail.com>
2019-07-01 19:13:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ class UI {
         // display cart item
         this.addCartItem(cartItem);
         // show the cart
+        this.showCart();
       });
     });
   }
@@ -112,7 +113,24 @@ class UI {
       <i class="fas fa-chevron-down" data-id=${item.id}></i>
     </div>`;
     cartContent.appendChild(div);
-    console.log(cartContent);
+  }
+  showCart() {
+    cartOverlay.classList.add("transparentBcg");
+    cartDOM.classList.add("showCart");
+  }
+  setupAPP() {
+    cart = Storage.getCart();
+    this.setCartValues(cart);
+    this.populateCart(cart);
+    cartBtn.addEventListener("click", this.showCart);
+    closeCartBtn.addEventListener("click", this.hideCart);
+  }
+  populateCart(cart) {
+    cart.forEach(item => this.addCartItem(item));
+  }
+  hideCart() {
+    cartOverlay.classList.remove("transparentBcg");
+    cartDOM.classList.remove("showCart");
   }
 }
 //local storage
@@ -127,10 +145,17 @@ class Storage {
   static saveCart() {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
+  static getCart() {
+    return localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
 }
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
+  //serup app
+  ui.setupAPP();
   //get all products
   products
     .getProducts()
